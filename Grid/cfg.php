f6b919755cc71dafaac31b791819baa6c8c5910c
dubr <dubr.cola@gmail.com>
2016-12-01 15:32:36 +0300

    super kolonki!

--- Grid/cfg.php
+++ Grid/cfg.php
@@ -4,7 +4,7 @@ use Floxim\Floxim\System\Fx as fx;
 return array(
     'actions' => array(
         'show' => array(
-            'name' => 'Супер-колонки'
+            'name' => 'Колонки'
         )
     )
 );
